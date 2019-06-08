1473f93912dfe4e5e1f88be8b6afd8177371f07f
Talgat S <talgat.s@protonmail.com>
2019-06-08 19:19:54 +0600

    move store to root/src/index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,38 +1,31 @@
-import React, {useReducer, useState} from 'react';
+import React, {useState} from 'react';
 import styles from './index.module.css';
-
-import {StateContext, DispatcherContext, reducer, initialState} from '>/state/';
 
 import JsonFormat from '>/components/JsonFormat/';
 import Builded from '>/components/Builded/';
 
-const App = () => {
-	const [state, dispatch] = useReducer(reducer, initialState);
+const App = ({state}) => {
 	const [isJson, setIsJson] = useState(false);
 
 	return (
 		<div className={styles.container}>
-			<DispatcherContext.Provider value={dispatch}>
-				<StateContext.Provider value={state}>
-					<main className={styles.main}>
-						{isJson ? (
-							<JsonFormat
-								data={state}
-								onBuild={() => {
-									setIsJson(false);
-								}}
-							/>
-						) : (
-							<Builded
-								data={state}
-								onJson={() => {
-									setIsJson(true);
-								}}
-							/>
-						)}
-					</main>
-				</StateContext.Provider>
-			</DispatcherContext.Provider>
+			<main className={styles.main}>
+				{isJson ? (
+					<JsonFormat
+						data={state}
+						onBuild={() => {
+							setIsJson(false);
+						}}
+					/>
+				) : (
+					<Builded
+						data={state}
+						onJson={() => {
+							setIsJson(true);
+						}}
+					/>
+				)}
+			</main>
 		</div>
 	);
 };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, {useReducer} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import {StateContext, DispatcherContext, reducer, initialState} from '>/state/';
+
+const AppWithStore = () => {
+	const [state, dispatch] = useReducer(reducer, initialState);
+	return (
+		<DispatcherContext.Provider value={dispatch}>
+			<StateContext.Provider value={state}>
+				<App state={state} />
+			</StateContext.Provider>
+		</DispatcherContext.Provider>
+	);
+};
+
+ReactDOM.render(<AppWithStore />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
