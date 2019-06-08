45fd36328c0e8d15be2d426b96cb1779ac625b46
Talgat S <talgat.s@protonmail.com>
2019-06-08 17:34:08 +0600

    Add AddButton component

---
+++ src/components/AddButton/AddButton.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from './AddButton.module.css';
+
+import {TYPE} from '>/state';
+
+import Button from '>/components/Button/';
+
+const AddButton = () => {
+	return (
+		<div className={styles.container}>
+			<Button>Add</Button>
+			<div className={styles['tooltip-buttons']}>
+				<Button>Box</Button>
+				<Button>Container</Button>
+			</div>
+		</div>
+	);
+};
+
+export default AddButton;
