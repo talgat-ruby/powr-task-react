72e7ca0ec675f6689f881c2d599856c662d097b1
Talgat S <talgat.s@protonmail.com>
2019-06-09 15:07:21 +0600

    use current key name

--- src/components/AddButton/AddButton.js
+++ src/components/AddButton/AddButton.js
@@ -5,17 +5,17 @@ import {DispatcherContext, ACTIONS} from '>/state/';
 
 import Button from '>/components/Button/';
 
-const AddButton = ({itemsKey}) => {
+const AddButton = ({currentKey}) => {
 	const dispatch = useContext(DispatcherContext);
 	return (
 		<div className={styles.container}>
 			<Button>Add</Button>
 			<div className={styles['tooltip-buttons']}>
-				<Button onClick={() => dispatch({type: ACTIONS.ADD_BOX, itemsKey})}>
+				<Button onClick={() => dispatch({type: ACTIONS.ADD_BOX, currentKey})}>
 					Box
 				</Button>
 				<Button
-					onClick={() => dispatch({type: ACTIONS.ADD_CONTAINER, itemsKey})}
+					onClick={() => dispatch({type: ACTIONS.ADD_CONTAINER, currentKey})}
 				>
 					Container
 				</Button>
